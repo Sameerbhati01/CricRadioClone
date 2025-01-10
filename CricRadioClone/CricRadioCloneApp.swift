60ec8b18d93efc6dc1dba8d0371bb10dc9df47ae
Sameer Bhati <bhati.sameer987@gmail.com>
2025-01-10 07:58:46 +0530

    Initial Commit

---
+++ CricRadioClone/CricRadioCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  CricRadioCloneApp.swift
+//  CricRadioClone
+//
+//  Created by Dhairya bhardwaj on 10/01/25.
+//
+
+import SwiftUI
+
+@main
+struct CricRadioCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
